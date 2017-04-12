9b6a34c78eeec4347b5b9b1d230f64c4c1de21b2
ch775 <ch775@nau.edu>
2017-04-11 17:23:58 -0700

    Carousel Done.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $('.carousel').slick({
-    
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    infintie: true,
+    pauseOnHover: false,
   });
 });
